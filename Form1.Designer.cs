40a5130916b8fc21d0a2afc2690f2710740d2608
Ugbodaga Augustine <83676555+Austine4real@users.noreply.github.com>
2023-06-22 18:25:33 +0100

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace SimpleCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
+            button3 = new System.Windows.Forms.Button();
+            button4 = new System.Windows.Forms.Button();
+            button5 = new System.Windows.Forms.Button();
+            button6 = new System.Windows.Forms.Button();
+            button7 = new System.Windows.Forms.Button();
+            button8 = new System.Windows.Forms.Button();
+            button9 = new System.Windows.Forms.Button();
+            button10 = new System.Windows.Forms.Button();
+            button12 = new System.Windows.Forms.Button();
+            button13 = new System.Windows.Forms.Button();
+            button14 = new System.Windows.Forms.Button();
+            button15 = new System.Windows.Forms.Button();
+            button16 = new System.Windows.Forms.Button();
+            button18 = new System.Windows.Forms.Button();
+            button19 = new System.Windows.Forms.Button();
+            button20 = new System.Windows.Forms.Button();
+            textBox_Result = new System.Windows.Forms.TextBox();
+            labelCurrentOperation = new System.Windows.Forms.Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button1.Location = new System.Drawing.Point(15, 172);
+            button1.Margin = new System.Windows.Forms.Padding(4);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(98, 59);
+            button1.TabIndex = 0;
+            button1.Text = "7";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button_click;
+            // 
+            // button2
+            // 
+            button2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button2.Location = new System.Drawing.Point(325, 172);
+            button2.Margin = new System.Windows.Forms.Padding(4);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(98, 59);
+            button2.TabIndex = 1;
+            button2.Text = "/";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += operator_click;
+            // 
+            // button3
+            // 
+            button3.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button3.Location = new System.Drawing.Point(221, 172);
+            button3.Margin = new System.Windows.Forms.Padding(4);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(98, 59);
+            button3.TabIndex = 2;
+            button3.Text = "9";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button_click;
+            // 
+            // button4
+            // 
+            button4.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button4.Location = new System.Drawing.Point(116, 172);
+            button4.Margin = new System.Windows.Forms.Padding(4);
+            button4.Name = "button4";
+            button4.Size = new System.Drawing.Size(98, 59);
+            button4.TabIndex = 3;
+            button4.Text = "8";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button_click;
+            // 
+            // button5
+            // 
+            button5.BackColor = System.Drawing.Color.Yellow;
+            button5.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button5.ForeColor = System.Drawing.Color.Black;
+            button5.Location = new System.Drawing.Point(430, 172);
+            button5.Margin = new System.Windows.Forms.Padding(4);
+            button5.Name = "button5";
+            button5.Size = new System.Drawing.Size(98, 59);
+            button5.TabIndex = 4;
+            button5.Text = "CE";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = System.Drawing.Color.Crimson;
+            button6.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button6.ForeColor = System.Drawing.Color.Black;
+            button6.Location = new System.Drawing.Point(430, 237);
+            button6.Margin = new System.Windows.Forms.Padding(4);
+            button6.Name = "button6";
+            button6.Size = new System.Drawing.Size(98, 59);
+            button6.TabIndex = 9;
+            button6.Text = "C";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button7.Location = new System.Drawing.Point(116, 237);
+            button7.Margin = new System.Windows.Forms.Padding(4);
+            button7.Name = "button7";
+            button7.Size = new System.Drawing.Size(98, 59);
+            button7.TabIndex = 8;
+            button7.Text = "5";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button_click;
+            // 
+            // button8
+            // 
+            button8.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button8.Location = new System.Drawing.Point(221, 237);
+            button8.Margin = new System.Windows.Forms.Padding(4);
+            button8.Name = "button8";
+            button8.Size = new System.Drawing.Size(98, 59);
+            button8.TabIndex = 7;
+            button8.Text = "6";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button_click;
+            // 
+            // button9
+            // 
+            button9.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button9.Location = new System.Drawing.Point(325, 237);
+            button9.Margin = new System.Windows.Forms.Padding(4);
+            button9.Name = "button9";
+            button9.Size = new System.Drawing.Size(98, 59);
+            button9.TabIndex = 6;
+            button9.Text = "*";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += operator_click;
+            // 
+            // button10
+            // 
+            button10.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button10.Location = new System.Drawing.Point(15, 237);
+            button10.Margin = new System.Windows.Forms.Padding(4);
+            button10.Name = "button10";
+            button10.Size = new System.Drawing.Size(95, 59);
+            button10.TabIndex = 5;
+            button10.Text = "4";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button_click;
+            // 
+            // button12
+            // 
+            button12.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button12.Location = new System.Drawing.Point(116, 303);
+            button12.Margin = new System.Windows.Forms.Padding(4);
+            button12.Name = "button12";
+            button12.Size = new System.Drawing.Size(98, 59);
+            button12.TabIndex = 13;
+            button12.Text = "2";
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += button_click;
+            // 
+            // button13
+            // 
+            button13.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button13.Location = new System.Drawing.Point(221, 303);
+            button13.Margin = new System.Windows.Forms.Padding(4);
+            button13.Name = "button13";
+            button13.Size = new System.Drawing.Size(98, 59);
+            button13.TabIndex = 12;
+            button13.Text = "3";
+            button13.UseVisualStyleBackColor = true;
+            button13.Click += button_click;
+            // 
+            // button14
+            // 
+            button14.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button14.Location = new System.Drawing.Point(325, 303);
+            button14.Margin = new System.Windows.Forms.Padding(4);
+            button14.Name = "button14";
+            button14.Size = new System.Drawing.Size(98, 59);
+            button14.TabIndex = 11;
+            button14.Text = "-";
+            button14.UseVisualStyleBackColor = true;
+            button14.Click += operator_click;
+            // 
+            // button15
+            // 
+            button15.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button15.Location = new System.Drawing.Point(15, 303);
+            button15.Margin = new System.Windows.Forms.Padding(4);
+            button15.Name = "button15";
+            button15.Size = new System.Drawing.Size(95, 59);
+            button15.TabIndex = 10;
+            button15.Text = "1";
+            button15.UseVisualStyleBackColor = true;
+            button15.Click += button_click;
+            // 
+            // button16
+            // 
+            button16.BackColor = System.Drawing.Color.Lime;
+            button16.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button16.ForeColor = System.Drawing.Color.Black;
+            button16.Location = new System.Drawing.Point(430, 303);
+            button16.Margin = new System.Windows.Forms.Padding(4);
+            button16.Name = "button16";
+            button16.Size = new System.Drawing.Size(98, 123);
+            button16.TabIndex = 19;
+            button16.Text = "=";
+            button16.UseVisualStyleBackColor = false;
+            button16.Click += button16_Click;
+            // 
+            // button18
+            // 
+            button18.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button18.Location = new System.Drawing.Point(221, 368);
+            button18.Margin = new System.Windows.Forms.Padding(4);
+            button18.Name = "button18";
+            button18.Size = new System.Drawing.Size(98, 59);
+            button18.TabIndex = 17;
+            button18.Text = ".";
+            button18.UseVisualStyleBackColor = true;
+            button18.Click += button_click;
+            // 
+            // button19
+            // 
+            button19.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button19.Location = new System.Drawing.Point(325, 368);
+            button19.Margin = new System.Windows.Forms.Padding(4);
+            button19.Name = "button19";
+            button19.Size = new System.Drawing.Size(98, 59);
+            button19.TabIndex = 16;
+            button19.Text = "+";
+            button19.UseVisualStyleBackColor = true;
+            button19.Click += operator_click;
+            // 
+            // button20
+            // 
+            button20.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button20.Location = new System.Drawing.Point(15, 368);
+            button20.Margin = new System.Windows.Forms.Padding(4);
+            button20.Name = "button20";
+            button20.Size = new System.Drawing.Size(201, 59);
+            button20.TabIndex = 15;
+            button20.Text = "0";
+            button20.UseVisualStyleBackColor = true;
+            button20.Click += button_click;
+            // 
+            // textBox_Result
+            // 
+            textBox_Result.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            textBox_Result.Location = new System.Drawing.Point(15, 100);
+            textBox_Result.Margin = new System.Windows.Forms.Padding(4);
+            textBox_Result.Name = "textBox_Result";
+            textBox_Result.Size = new System.Drawing.Size(514, 45);
+            textBox_Result.TabIndex = 20;
+            textBox_Result.Text = "0";
+            textBox_Result.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            textBox_Result.TextChanged += textBox_Result_TextChanged;
+            // 
+            // labelCurrentOperation
+            // 
+            labelCurrentOperation.AutoSize = true;
+            labelCurrentOperation.BackColor = System.Drawing.SystemColors.Control;
+            labelCurrentOperation.Font = new System.Drawing.Font("Segoe UI Light", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            labelCurrentOperation.Location = new System.Drawing.Point(15, 38);
+            labelCurrentOperation.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            labelCurrentOperation.Name = "labelCurrentOperation";
+            labelCurrentOperation.Size = new System.Drawing.Size(0, 38);
+            labelCurrentOperation.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            BackColor = System.Drawing.Color.FromArgb(224, 224, 224);
+            ClientSize = new System.Drawing.Size(544, 466);
+            Controls.Add(labelCurrentOperation);
+            Controls.Add(textBox_Result);
+            Controls.Add(button16);
+            Controls.Add(button18);
+            Controls.Add(button19);
+            Controls.Add(button20);
+            Controls.Add(button12);
+            Controls.Add(button13);
+            Controls.Add(button14);
+            Controls.Add(button15);
+            Controls.Add(button6);
+            Controls.Add(button7);
+            Controls.Add(button8);
+            Controls.Add(button9);
+            Controls.Add(button10);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+            Margin = new System.Windows.Forms.Padding(4);
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "Calculater";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button button20;
+        private System.Windows.Forms.TextBox textBox_Result;
+        private System.Windows.Forms.Label labelCurrentOperation;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculator
+{
+    public partial class Form1 : Form
+    {
+        Double resultValue = 0;
+        String operationPerformed = "";
+        bool isOperationPerformed = false;
+
+        string currentExpression = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (button.Text == ",")
+            {
+                if (!currentExpression.Contains(",") && currentExpression.Length > 0 && !isOperationPerformed)
+                {
+                    currentExpression += button.Text;
+                    textBox_Result.Text = FormatNumber(currentExpression);
+                }
+            }
+            else
+            {
+                currentExpression += button.Text;
+                textBox_Result.Text = FormatNumber(currentExpression);
+            }
+        }
+
+        private string FormatNumber(string number)
+        {
+            string strippedNumber = number.Replace(",", "");
+            double parsedNumber;
+
+            if (double.TryParse(strippedNumber, out parsedNumber))
+            {
+                return parsedNumber.ToString("N0");
+            }
+
+            return number;
+        }
+
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (!string.IsNullOrEmpty(currentExpression))
+            {
+                EvaluateExpression();
+            }
+
+            operationPerformed = button.Text;
+            currentExpression = ""; 
+            isOperationPerformed = true;
+
+            labelCurrentOperation.Text = resultValue + " " + operationPerformed;
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            EvaluateExpression();
+            operationPerformed = "";
+            labelCurrentOperation.Text = "";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            if (textBox_Result.Text.Length > 0)
+                textBox_Result.Text = textBox_Result.Text.Substring(0, textBox_Result.Text.Length - 1);
+
+            if (textBox_Result.Text.Length <= 0)
+                textBox_Result.Text = "0";
+            currentExpression = "";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox_Result.Text = "0";
+            currentExpression = "";
+            resultValue = 0;
+            labelCurrentOperation.Text = "";
+        }
+
+        private void EvaluateExpression()
+        {
+            if (isOperationPerformed && !string.IsNullOrEmpty(currentExpression))
+            {
+                double currentValue = Double.Parse(currentExpression);
+
+                try
+                {
+                    switch (operationPerformed)
+                    {
+                        case "+":
+                            resultValue += currentValue;
+                            break;
+                        case "-":
+                            resultValue -= currentValue;
+                            break;
+                        case "*":
+                            resultValue *= currentValue;
+                            break;
+                        case "/":
+                            resultValue /= currentValue;
+                            break;
+                        default:
+                            break;
+                    }
+
+                    if (double.IsInfinity(resultValue))
+                    {
+                        textBox_Result.Text = "Can't divide by 0";
+                    }
+                    else
+                    {
+                        textBox_Result.Text = resultValue.ToString("N0"); 
+                    }
+                }
+                catch (Exception)
+                {
+                    textBox_Result.Text = "Error";
+                }
+
+                currentExpression = "";
+            }
+            else
+            {
+                double currentValue = Double.Parse(textBox_Result.Text);
+                resultValue = currentValue;
+            }
+
+            isOperationPerformed = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_Result_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
